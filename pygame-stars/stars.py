5c6fc540440c8078f948bdadbae182823d8d89a9
Craig Giddings <randomhero_345@hotmail.com>
2024-10-07 21:08:51 -0400

    Added star class

--- pygame-stars/stars.py
+++ pygame-stars/stars.py
@@ -2,6 +2,22 @@ import pygame
 import sys
 
 
+class Star:
+    """A class to create a star"""
+
+    def __init__(self, screen):
+        self.surface = screen
+        self.color = (255, 255, 255)
+        self.center = (600, 400)
+        self.radius = 100
+    
+
+    def draw_star(self):
+        """Draw the star to the screen"""
+        pygame.draw.circle(self.surface, self.color, self.center, self.radius)
+
+
+
 def main():
     # Setup variables
     screen_width = 1200
@@ -13,6 +29,8 @@ def main():
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Starfield")
 
+    star = Star(screen)
+
     while True:
         # check for events
         for event in pygame.event.get():
@@ -21,6 +39,8 @@ def main():
 
         # Set the background color
         screen.fill(BG_COLOR)
+
+        star.draw_star()
 
         # Update the screen
         pygame.display.flip()
