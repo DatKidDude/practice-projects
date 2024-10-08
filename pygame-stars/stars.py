a96728ac00e543aabececac4d2fb8334411dec99
Craig Giddings <randomhero_345@hotmail.com>
2024-10-08 16:28:05 -0400

    Added ability to render multiple stars to screen

--- pygame-stars/stars.py
+++ pygame-stars/stars.py
@@ -1,20 +1,23 @@
 import pygame
+from pygame.sprite import Sprite, Group
 import sys
+import random
 
-
-class Star:
+class Star(Sprite):
     """A class to create a star"""
 
-    def __init__(self, screen):
-        self.surface = screen
-        self.color = (255, 255, 255)
-        self.center = (600, 400)
-        self.radius = 100
+    def __init__(self, screen, col, x, y, radius):
+        super().__init__()
+        self.screen = screen
+        self.color = col
+        self.x = x
+        self.y = y
+        self.radius = radius
     
 
-    def draw_star(self):
+    def update(self):
         """Draw the star to the screen"""
-        pygame.draw.circle(self.surface, self.color, self.center, self.radius)
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
 
 
 
@@ -23,24 +26,38 @@ def main():
     screen_width = 1200
     screen_height = 800
     BG_COLOR = (0, 0, 0)
+    STAR_COLOR = "white"
 
     # Initialize pygame, settings, and screen object
     pygame.init()
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Starfield")
+    
+    # Create a sprite group for stars
+    stars = Group()
 
-    star = Star(screen)
+    # Create a list of n stars
+    for _ in range(800):
+        # random (x, y) positions
+        x_pos = random.randint(1, screen_width)
+        y_pos = random.randint(1, screen_height)
+        # random radius size between 1.0 and 4.0
+        star_radius = float(random.randrange(100, 401))/100
+        star = Star(screen, STAR_COLOR, x_pos, y_pos, star_radius)
+        stars.add(star)
+
 
     while True:
         # check for events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                sys.exit()
-
+                sys.exit() 
+        
         # Set the background color
         screen.fill(BG_COLOR)
-
-        star.draw_star()
+        
+        # Draw stars on the screen
+        stars.update()
 
         # Update the screen
         pygame.display.flip()
